8ed44ebcb3316d01d30d8e783b2a558405bf3d47
latalavera <jesusfernandez0586@gmail.com>
2019-05-10 14:13:34 +0200

    first python exercise with else/if

---
+++ mood.py
@@ -0,0 +1,14 @@
+mood = input("How you are feeling today? " )
+
+if mood == "happy":
+    print("it is great to see you happy today")
+elif mood == "nervous":
+    print("take a deep breath 3 times")
+elif mood == "sad":
+    print("Smoke some weeed and it will cheer you up")
+elif mood == "excited":
+    print("So glad to to hear that")
+elif mood == "relaxed":
+    print("You should take advantge and go to bed right now")
+else:
+    print("I do not know what you mean")
